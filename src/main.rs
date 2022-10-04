285aa4f9c377a9d52943eba3fb4492fc171757e0
Aristide Fumo <aristide.fumo@sopth.com>
2022-10-04 22:26:37 +0200

    connect to the server but not good message

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
+use std::io::{Read, Write};
 use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
 
 
 fn main() {
     println!("Tententive de connexion au serveur ...");
-    /*match TcpStream::connect("127.0.0.1:7878") {
-        OK(_) => {
-            println!("Connexion au serveur réussie !");
-        }
-        Err(e) => {
-            println!("La connexion au serveur a échoué : {}", e);
-        }
-    }*/
     let ip = Ipv4Addr::new(127, 0, 0, 1);
     let port = 7878;
-
-    let tcp_s = TcpStream::connect(SocketAddrV4::new(ip, port));
-    let tcp_s = TcpStream::connect((ip, port));
-    let tcp_s = TcpStream::connect(("127.0.0.1", port));
-    let tcp_s = TcpStream::connect(("localhost", port));
-    let tcp_s = TcpStream::connect("127.0.0.1:7878");
-    let tcp_s = TcpStream::connect("localhost:7878");
+    if let Ok(stream) = TcpStream::connect("127.0.0.1:7878") {
+        println!("Connected to the server!");
+    } else {
+        println!("Couldn't connect to server...");
+    }
 }
+
