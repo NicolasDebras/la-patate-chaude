4c1176eeee177617b6a708e810de3e18cefeb74f
Aristide Fumo <aristide.fumo@sopth.com>
2022-11-01 21:15:56 +0100

    crazy

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-mod create_message;
-
-
-use std::io::{Read, Write};
+use std::io::{ Read, Write};
 use std::net::TcpStream;
+
+
 use crate::create_message::Message;
 
-fn main() {
-
-    let mut stream = TcpStream::connect("localhost:7878").expect("connection failed");
-
-    send_message(&stream, create_message::Message::Hello);
-    let mut text = String::new();
-    stream.read_to_string(&mut text).expect("read failed");
-    println!("got '{}'", text);
-
-
-
-
-
-    }
+mod create_message;
 
 pub fn send_message(mut stream: &TcpStream, message: Message) {
     let serialized = serde_json::to_string(&message).expect("failed to serialized object");
@@ -26,4 +12,33 @@ pub fn send_message(mut stream: &TcpStream, message: Message) {
 
     stream.write_all(&serialized_size.to_be_bytes()).expect("failed to send message size");
     stream.write_all(&serialized.as_bytes()).expect("failed to send message");
+}
+
+fn main_loop(mut stream: &TcpStream) {
+    let mut buf = [0; 4];
+
+    send_message(stream, Message::Hello);
+
+
+}
+
+fn buffer_to_object(message_buf: &mut Vec<u8>) -> Message {
+    let message = std::str::from_utf8(&message_buf).expect("failed to parse message");
+    //  println!("message: {message:?}");
+
+    let record: Message = serde_json::from_str(&message).expect("failed to serialize message");
+    //  println!("message: {record:?}");
+    record
+}
+
+fn main() {
+    let stream = TcpStream::connect("localhost:7878");
+
+
+    match stream {
+        Ok(stream) => {
+            main_loop(&stream);
+        }
+        Err(err) => panic!("Cannot connect: {err}"),
+    }
 }
